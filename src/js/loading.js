5f2dbab4f7553f8462c8806fcaed569ac1f97cc9
kim beom seok <qjatjr1285@naver.com>
2023-06-13 16:33:06 +0900

    로딩 이미지 파일 변경 및 코드 수정

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,8 +1,3 @@
-function test(imageName) {
-    LoadingWithMask('src\img\Spinner-1s-200px.gif');
-    setTimeout("closeLoadingWithMask()", 3000);
-}
-
 function LoadingWithMask(gif) {
     //화면의 높이와 너비를 구합니다.
     var maskHeight = $(document).height();
@@ -10,28 +5,28 @@ function LoadingWithMask(gif) {
     var body = document.querySelector('body')
 
     //화면에 출력할 마스크를 설정해줍니다.
-    var mask       = "<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+    let mask = `<div id='mask' style='position: fixed; z-index:1; background-color:#000000;
+                left:0; top:0; width: 100%; height: 100%; opacity: 0;'></div>`
     var loadingImg = '';
 
-    loadingImg += "<div id='loadingImg' style='position:absolute; top: calc(50% - (200px / 2)); width:100%; z-index:99999999;'>";
-    loadingImg += " <img src='src\img\Spinner-1s-200px.gif' style='position: relative; display: block; margin: 0px auto;'/>";
-    loadingImg += "</div>"; 
+    loadingImg += `<div id='loadingImg'>
+                    <img src='./src/img/LoadingImg.svg' style='position: fixed; z-index:2; top: 50%; left: 50%; transform: translate(-50%, -50%);'/>
+                </div>`
     //화면에 레이어 추가
     $('body')
         .append(mask)
-
+        .append(loadingImg)
     //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
     $('#mask').css({
             'width' : maskWidth,
             'height': maskHeight,
-            'opacity' : '0.3'
+            'opacity' : '0'
     }); 
 
     //마스크 표시
     $('#mask').show();
 
     //로딩중 이미지 표시
-    $('#loadingImg').append(loadingImg);
     $('#loadingImg').show();
 }
 
