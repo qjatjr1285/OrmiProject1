d28c4f758dc283bf4ca6473cf2eb532ffe22c65a
kim beom seok <qjatjr1285@naver.com>
2023-06-13 15:58:15 +0900

    로딩창 js

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,38 +1,40 @@
 function test(imageName) {
-    LoadingWithMask('Spinner-1s-200px.gif');
+    LoadingWithMask('src\img\Spinner-1s-200px.gif');
     setTimeout("closeLoadingWithMask()", 3000);
 }
- 
+
 function LoadingWithMask(gif) {
     //화면의 높이와 너비를 구합니다.
     var maskHeight = $(document).height();
     var maskWidth  = window.document.body.clientWidth;
-     
+    var body = document.querySelector('body')
+
     //화면에 출력할 마스크를 설정해줍니다.
     var mask       = "<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
     var loadingImg = '';
-      
-    loadingImg += " <img src='"+ gif + "' style='position: absolute; display: block; margin: 0px auto;'/>";
- 
+
+    loadingImg += "<div id='loadingImg' style='position:absolute; top: calc(50% - (200px / 2)); width:100%; z-index:99999999;'>";
+    loadingImg += " <img src='src\img\Spinner-1s-200px.gif' style='position: relative; display: block; margin: 0px auto;'/>";
+    loadingImg += "</div>"; 
     //화면에 레이어 추가
     $('body')
         .append(mask)
- 
+
     //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
     $('#mask').css({
             'width' : maskWidth,
             'height': maskHeight,
             'opacity' : '0.3'
     }); 
-  
+
     //마스크 표시
     $('#mask').show();
-  
+
     //로딩중 이미지 표시
     $('#loadingImg').append(loadingImg);
     $('#loadingImg').show();
 }
- 
+
 function closeLoadingWithMask() {
     $('#mask, #loadingImg').hide();
     $('#mask, #loadingImg').empty();  
